ca284a6beda85f034e42d5f0e8a389f1d8fd241b
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-09-24 18:43:47 +0100

    added swagger documentation

--- src/main/java/com/capstoneproject/ElitesTracker/ElitesTrackerApplication.java
+++ src/main/java/com/capstoneproject/ElitesTracker/ElitesTrackerApplication.java
@@ -2,8 +2,34 @@ package com.capstoneproject.ElitesTracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "ELites Tracker",
+				version = "v1",
+				description = "This app provides REST APIs for capstone project",
+				contact = @Contact(
+						name = "Odogwu Legends",
+						email = ""
+				)
+		),
+		servers = {
+				@Server(
+						url = "http://localhost:8092",
+						description = "DEV Server"
+				),
+				@Server(
+						url = "https://elitestracker-production.up.railway.app",
+						description = "PROD server"
+				)
+		}
+)
 public class ElitesTrackerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ElitesTrackerApplication.class, args);
