2e7f9ed91f32cb491b99c94f601d0b19cd553c50
OdogwuLegends <odigbocharlesjnrlegends@gmail.com>
2023-10-05 12:46:21 +0100

    added new files

--- src/main/java/com/capstoneproject/ElitesTracker/repositories/AttendanceRepository.java
+++ src/main/java/com/capstoneproject/ElitesTracker/repositories/AttendanceRepository.java
@@ -3,12 +3,25 @@ package com.capstoneproject.ElitesTracker.repositories;
 import com.capstoneproject.ElitesTracker.models.Attendance;
 import com.capstoneproject.ElitesTracker.models.EliteUser;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface AttendanceRepository extends JpaRepository<Attendance,Long> {
     Optional<Attendance> findByIpAddress(String ipAddress);
-    Optional<Attendance> findByIpAddressAndDateTaken(String ipAddress,String date);
+//    Optional<Attendance> findByIpAddressAndDateTaken(String ipAddress,String dateTaken);
+
+    @Query("SELECT a FROM attendance a WHERE a.ipAddress = :ipAddress AND a.dateTaken = :dateTaken")
+//@Query(
+//        value = "SELECT * FROM attendance WHERE ip_address = :ipAddress AND date_taken = :dateTaken LIMIT 1",
+//        nativeQuery = true
+//)
+    Optional<Attendance> findByIpAddressAndDateTaken(
+            @Param("ipAddress") String ipAddress,
+            @Param("dateTaken") String dateTaken
+    );
+
     Optional<Attendance> findByCohort(String cohort);
     Optional<Attendance> findByUser(EliteUser eliteUser);
 
